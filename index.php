f3e3cb92618e8bd0ea789fe44f5f2e27a1a753ff
chinnawut.vgrouphonda <chinnawut.vgrouphonda@gmail.com>
2019-11-11 16:34:03 +0700

    test

--- index.php
+++ index.php
@@ -26,8 +26,17 @@ require 'vendor/autoload.php';
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('PK22vMsinZQh0VWpwVlHnXCXiHfDoA+oGk1d0eaOaIRtPf6mEIDvRVprJ0e7o06eKjqa2B3TONZ9CkOP2og96CHl1v21hcmoB5mwZm1umzoHRy0zkyFPEC3kkwKXuO9IECWdEc0zz/3GMcoExpVy9gdB04t89/1O/w1cDnyilFU=');
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '3ac88d67a3c8f207a35717e4537f6f58']);
 
-foreach($data['event'] as $even){
-$response = $bot->replyText('<reply token>', 'hello!');
-}
+foreach ($data['events'] as $event)
+ {
+  $userMessage = $event['message']['text'];
+  if(strtolower($userMessage) == 'halo')
+  {
+   $message = "Halo juga";
+    $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($message);
+   $result = $bot->replyMessage($event['replyToken'], $textMessageBuilder);
+   return $result->getHTTPStatus() . ' ' . $result->getRawBody();
+  
+  }
+ }
 
 ?>
