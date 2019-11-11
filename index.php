dab602fabf6d70e5b5b9ec4311369d1fe9dd1b90
chinnawut.vgrouphonda <chinnawut.vgrouphonda@gmail.com>
2019-11-11 16:28:53 +0700

    tet

--- index.php
+++ index.php
@@ -26,6 +26,8 @@ require 'vendor/autoload.php';
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('PK22vMsinZQh0VWpwVlHnXCXiHfDoA+oGk1d0eaOaIRtPf6mEIDvRVprJ0e7o06eKjqa2B3TONZ9CkOP2og96CHl1v21hcmoB5mwZm1umzoHRy0zkyFPEC3kkwKXuO9IECWdEc0zz/3GMcoExpVy9gdB04t89/1O/w1cDnyilFU=');
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '3ac88d67a3c8f207a35717e4537f6f58']);
 
+foreach($data['event'] as $even){
 $response = $bot->replyText('<reply token>', 'hello!');
+}
 
 ?>
