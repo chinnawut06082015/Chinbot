c109e1e597578d15556e18f98791c543ee01eefa
chinnawut.vgrouphonda <chinnawut.vgrouphonda@gmail.com>
2019-11-12 09:16:13 +0700

    test

--- index.php
+++ index.php
@@ -4,18 +4,14 @@ require_once 'bot_settings.php';
 /*
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('PK22vMsinZQh0VWpwVlHnXCXiHfDoA+oGk1d0eaOaIRtPf6mEIDvRVprJ0e7o06eKjqa2B3TONZ9CkOP2og96CHl1v21hcmoB5mwZm1umzoHRy0zkyFPEC3kkwKXuO9IECWdEc0zz/3GMcoExpVy9gdB04t89/1O/w1cDnyilFU=');
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '3ac88d67a3c8f207a35717e4537f6f58']);*/
-
-// กรณีต้องการตรวจสอบการแจ้ง error ให้เปิด 3 บรรทัดล่างนี้ให้ทำงาน กรณีไม่ ให้ comment ปิดไป
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
  
 // include composer autoload
 require_once '/vendor/autoload.php';
- 
+
 // กรณีมีการเชื่อมต่อกับฐานข้อมูล
 //require_once("dbconnect.php");
- 
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(LINE_MESSAGE_ACCESS_TOKEN);
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => LINE_MESSAGE_CHANNEL_SECRET]);
 // เชื่อมต่อกับ LINE Messaging API
 $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello');
 $response = $bot->pushMessage('<to>', $textMessageBuilder);
@@ -40,6 +36,4 @@ if ($response->isSucceeded()) {
     echo 'Succeeded!';
     return;
 }*/
-// Failed
-echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
 ?>
