26ecaf73ff524a90f1f49b3d1a93c8fbb78d017a
chinnawut.vgrouphonda <chinnawut.vgrouphonda@gmail.com>
2019-11-12 14:40:12 +0700

    test

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -7,6 +7,14 @@ namespace Composer\Autoload;
 class ComposerStaticInitb05b07924e58282348c9adb350e04daf
 {
     public static $prefixLengthsPsr4 = array (
+        'P' => 
+        array (
+            'Psr\\Log\\' => 8,
+        ),
+        'M' => 
+        array (
+            'Monolog\\' => 8,
+        ),
         'L' => 
         array (
             'LINE\\' => 5,
@@ -14,6 +22,14 @@ class ComposerStaticInitb05b07924e58282348c9adb350e04daf
     );
 
     public static $prefixDirsPsr4 = array (
+        'Psr\\Log\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/psr/log/Psr/Log',
+        ),
+        'Monolog\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/monolog/monolog/src/Monolog',
+        ),
         'LINE\\' => 
         array (
             0 => __DIR__ . '/..' . '/linecorp/line-bot-sdk/src',
