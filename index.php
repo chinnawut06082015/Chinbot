20bf98db22f05d571829388e94db3f4fd390fe9e
chinnawut.vgrouphonda <chinnawut.vgrouphonda@gmail.com>
2019-11-12 14:43:54 +0700

    test

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 DEFINE("ACCESS_TOKEN","PK22vMsinZQh0VWpwVlHnXCXiHfDoA+oGk1d0eaOaIRtPf6mEIDvRVprJ0e7o06eKjqa2B3TONZ9CkOP2og96CHl1v21hcmoB5mwZm1umzoHRy0zkyFPEC3kkwKXuO9IECWdEc0zz/3GMcoExpVy9gdB04t89/1O/w1cDnyilFU=");
 DEFINE("SECRET_TOKEN","3ac88d67a3c8f207a35717e4537f6f58");
 
-require_once '../vendor/autoload.php';
+require_once 'D:\php\vendor\autoload.php';
 use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
 use Monolog\Handler\FirePHPHandler;
